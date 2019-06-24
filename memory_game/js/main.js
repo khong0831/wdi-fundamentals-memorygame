4551fdd376322f1816cd698f6cd7a2d68961cd51
khong0831 <51841523+khong0831@users.noreply.github.com>
2019-06-23 19:02:35 -0700

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardone = "queen";
+var cardtwo = "queen";
+var cardthree = "king";
+var cardfour = "king";
+console.log("User flipped " + cardone);
+console.log("User flipped " + cardthree);
