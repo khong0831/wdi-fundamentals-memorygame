14ecfdfdb510d9311cac5a41811ffd11d45eab4d
khong0831 <51841523+khong0831@users.noreply.github.com>
2019-06-24 03:22:06 -0700

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
 console.log("Up and running!");
-var cardone = "queen";
-var cardtwo = "queen";
-var cardthree = "king";
-var cardfour = "king";
-console.log("User flipped " + cardone);
-console.log("User flipped " + cardthree);
+console.log("user flipped" + cardone);
+console.log("user flipped" + cardtwo);
+
+var cards = ["queen", "queen", "king", "king"]
+var cardsinplay = [];
+
+var cardone = cards[0];
+cardsinplay.push("queen");
+console.log("user flipped queen");
+
+var cardtwo = cards[3];
+cardsinplay.push("king");
+console.log("user flipped king");
+
+if (cardsinplay.length === 2) {
+  if (cardsinplay[0] === cardsinplay[1]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.")
+  }
+}
